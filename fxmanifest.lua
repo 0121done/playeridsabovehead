878052d8fb45817642400be2ffe953f8b5dec9af
0121done <95552895+0121done@users.noreply.github.com>
2021-12-05 01:03:56 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+-- Credits 0121done
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author '0121done'
+description 'IDsAboveHead'
+version '1.0.0'
+
+client_script "cl_idsabovehead.lua"
